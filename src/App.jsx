4b984c1d19f3ea81ced4d3c71cc03024a5212812
alisami0111 <saminagri0111@gmail.com>
2024-04-18 21:57:53 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="box">
+        <span className="title">Ali Sami</span>
+        <div>
+          <strong>Allied Bank</strong>
+          <p>0000 000 000 0000</p>
+          <span>VALID</span> <span>01/28</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
